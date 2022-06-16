4dcc6b745a255c22588531c5eae723442527279a
Geovan Goes <geovansilvagoes@gmail.com>
2022-06-15 22:10:33 -0300

    flow

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from'./Botao.module.scss';
+import Iprops from '../IProps';
+
+class Botao extends React.Component<{
+    children?: React.ReactNode,
+    type?: "button" | "submit" | "reset" | undefined
+}> {
+    render() {
+        const { type = "button" } = this.props;
+        return (
+            <button type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,21 @@
+import Botao from '../Botao'
+import Relogio from './Relogio'
+import style from './Cronometro.module.scss';
+
+import { tempoParaSegundos } from '../../common/utils/Date';
+
+
+export default function Cronometro() {
+    console.log(tempoParaSegundos('01:01:01'));
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
+            <div className={style.relogioWrapper}>
+                <Relogio/>
+           </div>
+           <Botao>
+              Começar!
+           </Botao>
+       </div>
+      )
+}
